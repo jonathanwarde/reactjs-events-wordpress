c8cc283b7ff5df5989aa37549ae8ae0d8890479c
Jon Warde <jon.warde@7dots.co.uk>
2025-04-24 13:50:12 +0100

    react date picker component

--- src/vitereacttopsecretevents.jsx
+++ src/vitereacttopsecretevents.jsx
@@ -1,8 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import {CalendarDatePicker} from './js/reactcomponents/Calendar';
 
 function App() {
-  return <div>Entry point for events...</div>;
+  return <div className="text-2xl font-heading">
+    <CalendarDatePicker />
+    Entry point for events...
+    </div>;
 }
 const element = document.getElementById('react-root');
 if (element) {
