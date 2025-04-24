d6fa75d1bd4dac4dfb67ede6905a6e445915ff1d
Jon Warde <jon.warde@7dots.co.uk>
2025-04-24 12:21:54 +0100

    add menu classes

--- functions/addMenu.php
+++ functions/addMenu.php
@@ -3,6 +3,13 @@
 function register_menu() {
   register_nav_menu('main-menu',__( 'Main menu' ));
 }
-add_action( 'init', 'register_menu' )
-	
-?>
+add_action( 'init', 'register_menu' );
+
+function add_custom_class_to_menu_links($attributes, $item, $args) {
+  if ($args->theme_location == 'main-menu') {
+      $attributes['class'] = 'menu-item'; 
+  }
+  return $attributes;
+}
+add_filter('nav_menu_link_attributes', 'add_custom_class_to_menu_links', 10, 3);
+
