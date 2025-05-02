b6a31e19d6259fd59ab70724f03f2a1ff7bd1c32
Jon Warde <jon.warde@7dots.co.uk>
2025-05-02 14:59:53 +0100

    map events from fetch to <ol> of EventCards

--- src/vitereacttopsecretevents.jsx
+++ src/vitereacttopsecretevents.jsx
@@ -1,13 +1,64 @@
-import React from 'react';
+import React, { useState } from 'react';
 import ReactDOM from 'react-dom';
 import { CalendarDatePicker } from './js/reactcomponents/calendar/Calendar.jsx';
+import { EventCard } from './js/reactcomponents/calendar/EventCard.jsx';
 
+const App = () => {
+  const [fetchedData, setFetchedData] = useState({ events: [] });
+  const [selectedDate, setSelectedDate] = useState('Today');
 
-function App() {
-  return <div className="">
-    <CalendarDatePicker />
-    </div>;
-}
+  //format date to 2025-04-26 format
+  const formatDate = (input) => {
+    const [, dmy] = input.split(",");
+    const withYear = dmy.trim() + " " + new Date().getFullYear();
+    const dt = new Date(withYear);
+    if (isNaN(dt)) throw new Error("Invalid date");
+    const y = dt.getFullYear();
+    const m = String(dt.getMonth() + 1).padStart(2, "0");
+    const d = String(dt.getDate()).padStart(2, "0");
+    return `${y}-${m}-${d}`;
+  }
+  
+
+  const fetchData = async (date) => {
+    const dateFormatted = formatDate(date)
+    try {
+      const response = await fetch('https://tscc.local/web/wp-admin/admin-ajax.php', {
+        method: 'POST',
+        headers: {
+          'accept': 'application/json, text/plain, */*',
+          'content-type': 'application/x-www-form-urlencoded;charset=UTF-8',
+        },
+        body: `action=jw_events_by_date_v2&date=${dateFormatted}`,
+      });
+
+      const result = await response.json();
+      setFetchedData(result);
+    } catch (error) {
+      console.error('Error fetching data:', error);
+    }
+  };
+
+  const listFetchedEvents = fetchedData.events.map(_event =>
+    <EventCard title={_event.title} />
+  );
+
+  return (
+    <>
+      <CalendarDatePicker
+        selectedDate={selectedDate}
+        setSelectedDate={setSelectedDate}
+        fetchData={fetchData}
+      />
+      {fetchedData && (
+        <ol className="grid gap-[30px] w-full md:[grid-template-columns:50fr_50fr]">
+           {listFetchedEvents}
+        </ol>
+      )}
+    </>
+  );
+};
+
 const element = document.getElementById('react-root');
 if (element) {
   ReactDOM.render(<App />, element);
